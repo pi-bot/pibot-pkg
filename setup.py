ebf17f87159c0aae983b83d8efaba37780aa2db1
Harry Gee <code@pibot.org>
2016-08-15 15:42:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'sample_data': ['sample_data.json'],
+        'data': ['sample_data.json'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
