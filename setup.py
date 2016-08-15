29296175270d03eeaaed843ad0e01bd21041df53
Harry Gee <code@pibot.org>
2016-08-15 15:24:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     author_email='Harry@pibot.org',
 
     # Choose your license
-    license='License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    license='GNU General Public License v3 or later (GPLv3+)',
     
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -50,7 +50,7 @@ setup(
         'Topic :: Education',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL3 License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
